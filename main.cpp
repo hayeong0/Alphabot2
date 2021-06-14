a0d3a9df1373954d142675f2dccea5e821ab9197
Hayeong Choi <21800758@handong.edu>
2021-06-14 21:06:52 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #define NUM_COLORS 6
 #define NUM_LEDS_PER_COLOR 4
 
+//125, 255, 300, 350
 #define maximum 255
 #define PWMA 0.3
 #define PWMB 0.3
@@ -39,11 +40,14 @@ int colorbuf[NUM_COLORS] = {0x2f0000, 0x2f2f00, 0x002f00, 0x002f2f, 0x00002f, 0x
 int button = 0;
 int pos = 0;
 
-int last_proportional = 0;
+int position = 0;
+//double power_diff = 0.0;
+int prev_proportional = 0;
 int integral = 0;
 int dist = 0;
-float time = 0;
-float kp = 20.0, ki = 0.0001, kd = 10.0;
+float sum = 0;
+float kp = 3.0, ki = 0.0, kd = 0.0;
+//float ptmp = 3.0, itmp = 0.0, dtmp = 0.0;
 
 void RGB() {
   // RGB setting
@@ -51,19 +55,48 @@ void RGB() {
         px.Set(i, colorbuf[(i / NUM_LEDS_PER_COLOR) % NUM_COLORS]);
     }
     for (int j = 0; j < WS2812_BUF; j++) {
-        px.SetI(j%WS2812_BUF, 0x4B);
+        px.SetI(j % WS2812_BUF, 0x4B);
         ThisThread::sleep_for(1);
     }
-
     for (int i = WS2812_BUF; i >= 0; i--) {
       ws.write_offsets(px.getBuf(), i, i, i);
     }
 }
-void display() {
+
+void display_time() {
   gOLED.clearDisplay();
-  gOLED.setTextCursor(0,0);
+  gOLED.setTextCursor(0, 0);
   gOLED.printf("** Alphabot **\r\n");
-  gOLED.printf("Time: %f **\r\n", time);      
+  gOLED.printf("Time: %f (sec) **\r\n", sum);      
+  gOLED.display();
+}
+
+// void display_diff() {
+//  gOLED.clearDisplay();
+//  gOLED.setTextCursor(0, 0);
+//  gOLED.printf("diff:  %ld \r\n", power_diff);      
+//  gOLED.display();
+// }
+
+void display_val() {
+  gOLED.clearDisplay();
+  gOLED.setTextCursor(0, 0);
+  gOLED.printf("[ %d ]\r\n", pos);      
+  gOLED.display();
+}
+
+void display_position() {
+  gOLED.clearDisplay();
+  gOLED.setTextCursor(0, 0);
+  gOLED.printf("[ %d ]\r\n", position);      
+  gOLED.display();
+}
+void display_info() {
+  gOLED.clearDisplay();
+  gOLED.setTextCursor(2, 2);
+  gOLED.printf("P : %f\r\n", ptmp);
+  gOLED.printf("I : %f\r\n", itmp);  
+  gOLED.printf("D : %f\r\n", dtmp);       
   gOLED.display();
 }
     
@@ -76,7 +109,7 @@ int main() {
   
     // OLEd
     i2c.frequency(100000);      
-    display();
+    display_time();
 
     // IR remote
     while(1) { 
@@ -88,25 +121,50 @@ int main() {
         }
         // TODO: IR 값 정밀 체크해주는 부분 추가하기
         switch(button) {
+            // Button + (Increase P val)
+            case 0x15: 
+              ptmp += 0.02;
+              display_info();
+              button = 0x09;
+              break;
+            
+            // Button - (Increase D val)
+            case 0x07: 
+              dtmp += 0.02;
+              display_info();
+              button = 0x09;
+              break;
+            
             // Button >|| (Calibration): 0x00FF43BC
             case 0x43: {
               //motorDriver.setspeed(0.6, 0.6);
-              int i = 20;
+              int i = 50;
               sprintf(buffer, "[*] calibration start!\r\n");
               pc.write(buffer, strlen(buffer));
+          
               while (i--) {
                 motorDriver.forward(0.5, 0.5);
                 tr.calibrate(); 
                 ThisThread::sleep_for(100);
                 motorDriver.backward(0.5, 0.5);
                 tr.calibrate();
-                ThisThread::sleep_for(100);
-                motorDriver.turn_left(0.5, 0.2);
-                tr.calibrate();
-                ThisThread::sleep_for(100);
+                ThisThread::sleep_for(100);      
+                // ThisThread::sleep_for(100);
+                // motorDriver.turn_left(0.4, 0.5);
+                // tr.calibrate();
+                // ThisThread::sleep_for(100);
+                // motorDriver.turn_right(0.5, 0.4);
+                // tr.calibrate();
               }
+              motorDriver.stop();
               sprintf(buffer, "[-] calibration done!\r\n");
               pc.write(buffer, strlen(buffer));
+
+              gOLED.clearDisplay();
+              gOLED.setTextCursor(0, 0);
+              gOLED.printf("[*] Calibration Done! \r\n");       
+              gOLED.display();
+
               ThisThread::sleep_for(200);
               button = 0x09;       
               break;
@@ -138,6 +196,8 @@ int main() {
             // Button 6 (Turn right) : 0x00FF5AA5
             case 0x5A:
                 motorDriver.turn_right(0.3, 0.1);
+                //motorDriver.turn_right(0.3, 0.1);
+                tr.calibrate();
                 ThisThread::sleep_for(100);
                 button = 0x09;        
                 break;
@@ -150,69 +210,91 @@ int main() {
             // Button 5 (Auto Drive) : 0x00FF1CE3
             // 라인 위치 파악 + 모터 제어
             case 0x1C: {
-             
                 int start = 0, end = 0;
-             
                 //ultra.init();        
                 t.reset();
-                t.start()
+                t.start();
                 start = t.elapsed_time().count();
 
-                while(1) {  
-                    
-                    int position = tr.readLine(sensor_values, 0);
+                while(1) {       
+                    position = tr.readLine(sensor_values, 0);
                     ultra.start();
                     dist = ultra.get_dist_cm();
                   
                     if (dist <= 20) {
                       sprintf(buffer, "*=== bbanzzak! bbanzzak! ===*\r\n");
                       pc.write(buffer, strlen(buffer));
-
+                      // OLED
+                      gOLED.clearDisplay();
+                      gOLED.setTextCursor(0, 0);
+                      gOLED.printf("[*] Done! \r\n");       
+                      gOLED.display();
                       // RGB
-                      RGB();
+                      //RGB();
                       break;
                     }
 
                     // 선이 IR1 왼쪽에 있는 상태
-                    if (position == 0) {
-                      motorDriver.turn_left(0.1, 0.3);
-                    }
-                    else if (position > 0 && position < 800) {
-                      motorDriver.turn_left(0.2, 0.3);
-                    }
-                    else if (position > 800 && position < 2700) {
-                      motorDriver.forward(0.3, 0.3);
-                    }
-                    else if(position > 2700 && position < 3000) {
-                      motorDriver.turn_right(0.3, 0.3);
-                    }
-                    else if (position <= 3500 && position >= 3000) {
-                      motorDriver.turn_right(0.3, 0.15);
-                    }
-                    else if (position <= 4000 && position > 3500) {
-                      motorDriver.turn_right(0.3, 0.1);
-                    }
+                    // if (position == 0) {
+                    //   motorDriver.turn_left(0.1, 0.2);
+                    // }
+                    // else if (position > 0 && position < 650) {
+                    //   motorDriver.turn_left(0.13, 0.2);
+                    // }
+                    // else if (position >= 650 && position < 1500) {
+                    //   motorDriver.turn_left(0.17, 0.2);
+                    // }
+                    // else if (position >= 1500 && position < 2700) {
+                    //   motorDriver.forward(0.2, 0.2);
+                    // }
+                    // else if(position >= 2700  && position < 3500) {
+                    //   motorDriver.turn_right(0.2, 0.15);
+                    // }
+                    // else if (position >= 3500 && position < 4000) {
+                    //   motorDriver.turn_right(0.25, 0.15);
+                    // }
+                    // else if (position > 4000) {
+                    //   motorDriver.turn_right(0.3, 0.15);
+                    // }
 
-                    int proportional = position - 2000; // 오차
-                    int derivative = proportional-last_proportional;  // 현재 오차-이전 오차
-                    integral += proportional; // 오차 누적
-                    last_proportional = proportional; 
+                    int proportional = position - 2000; // P: 오차
+                    int derivative = proportional - prev_proportional;  // D: 현재 오차-이전 오차
+                    integral += proportional; // I: 오차 누적
+                    prev_proportional = proportional; 
 
                     int power_diff = proportional/kp + integral*ki + derivative*kd; //pid 적용 후 제어 값
-
+                    
+                    sprintf(buffer, "P : %f\r\n", kp);
+                    pc.write(buffer, strlen(buffer));
+                    sprintf(buffer, "I : %f\r\n", ki);
+                    pc.write(buffer, strlen(buffer));
+                    sprintf(buffer, "D : %f\r\n", kd);
+                    pc.write(buffer, strlen(buffer));
                     sprintf(buffer, "[+] Power_diff: %d\r\n", power_diff);
                     pc.write(buffer, strlen(buffer));
+                    
+                    gOLED.clearDisplay();
+                    gOLED.setTextCursor(2, 2);
+                    gOLED.printf("P : %f\r\n", ptmp);
+                    gOLED.printf("I : %f\r\n", itmp);  
+                    gOLED.printf("D : %f\r\n", dtmp);     
+                    gOLED.printf("P.D. : %d\r\n", power_diff);     
+                    gOLED.display();
+
 
                     if (power_diff > maximum) power_diff = maximum;
                     if (power_diff < -maximum) power_diff = -maximum;
 
-                    // const float std = 970.0
                     // // saturate
-                    if (power_diff < 0) { // 왼쪽으로 치우침 --> 오른쪽 바퀴 가속
-                      motorDriver.forward(PWMA+(float)power_diff/maximum*PWMA, PWMB);               
+                    if (power_diff < 0) { // 오른쪽으로 치우침 --> 오른쪽 바퀴 가속
+                      motorDriver.forward(PWMA+(float)power_diff/maximum*PWMA, PWMB); 
+                      // if (power_diff == -maximum) motorDriver.forward(0.2, PWMB);      
+                      // else motorDriver.forward(PWMA+(float)power_diff/maximum*PWMA, PWMB);           
                     }
-                    else if (power_diff == 0) {
+                    else if (power_diff > 0) {  // 중앙보다 왼쪽에 위치 --> 왼쪽 바퀴 가속
                       motorDriver.forward(PWMA, PWMB-(float)power_diff/maximum*PWMB);
+                      // if (power_diff == maximum) motorDriver.forward(PWMA, 0.2);
+                      // else motorDriver.forward(PWMA, PWMB-(float)power_diff/maximum*PWMB);  
                     }
                     else {
                       motorDriver.forward(PWMA, PWMB);
@@ -221,21 +303,27 @@ int main() {
                     // debug
                     sprintf(buffer, "[+] Position: %d\r\n", position);
                     pc.write(buffer, strlen(buffer));
+                    //display_position();
                     ThisThread::sleep_for(100);
 
                     // position이 왼쪽이나 오른쪽으로 치우쳐 있을 때
-                    if (sensor_values[0] > 800 && sensor_values[1] > 800 && sensor_values[2] > 800) {
-                      motorDriver.forward(0, 0);
-                      motorDriver.stop();
+                    // if (sensor_values[0] > 800 && sensor_values[1] > 800 && sensor_values[2] > 800) {
+                    //   motorDriver.forward(0, 0);
+                    //   motorDriver.stop();
                      
-                      end = t.elapsed_time().count();
-                      time = end-start;
-                      display();
-                      RGB();
-                      ThisThread::sleep_for(1500);
-                      break;
-                    }
+                    //   end = t.elapsed_time().count();
+                    //   sum = end-start;
+                    //   display();
+                    //   RGB();
+                    //   ThisThread::sleep_for(1500);
+                    //   break;
+                    // }
                 }
+                end = t.elapsed_time().count();
+                sum = end-start;
+                sum /= 1e+6;
+
+                display_time();
                 button = 0x09;   
                 break;  
             }
@@ -243,15 +331,15 @@ int main() {
             // Button 100+ (Sensor value) : 0x00FF19E6
             case 0x19: {
               tr.AnalogRead(sensor_values);
-                for (int i = 0; i < 5; i++) {
-                    sprintf(buffer, "IR[%d] %d\r\n", i+1, sensor_values[i]);
-                    pc.write(buffer, strlen(buffer));
-                }
+              for (int i = 0; i < 5; i++) {
+                  sprintf(buffer, "IR[%d] %d\r\n", i+1, sensor_values[i]);
+                  pc.write(buffer, strlen(buffer));
+              }
               
-                sprintf(buffer, "[*] Done!\r\n");
-                pc.write(buffer, strlen(buffer));
-                button = 0x09;       
-                break; 
+              sprintf(buffer, "[*] Done!\r\n");
+              pc.write(buffer, strlen(buffer));
+              button = 0x09;       
+              break; 
             }
                    
 
@@ -261,16 +349,21 @@ int main() {
               for (int i = 0; i < 5; i++) {
                     sprintf(buffer, "Calib[%d] %d\r\n", i+1, sensor_values[i]);
                     pc.write(buffer, strlen(buffer));
+
+
+
                 }
 
               int j = 50;
-                while (j--) {
-                    pos = tr.readLine(sensor_values, 0);
-                    sprintf(buffer, "val: %d\r\n", pos);
-                    pc.write(buffer, strlen(buffer));
-                } 
-                button = 0x09;       
-                break;  
+              while (j--) {
+                  pos = tr.readLine(sensor_values, 0);
+                  sprintf(buffer, "val: %d\r\n", pos);
+                  pc.write(buffer, strlen(buffer));
+              } 
+              pos = tr.readLine(sensor_values, 0);
+              display_val();
+              button = 0x09;       
+              break;  
             }
             
             default: {
@@ -281,3 +374,4 @@ int main() {
       } //end of switch
     } // end of while
 }
+ 
